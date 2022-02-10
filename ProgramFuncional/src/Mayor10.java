f73c1b461653c544497ac1195ff30fd33e616850
Francisco Alcalde <95127205+fran86alcalde@users.noreply.github.com>
2022-02-10 10:11:43 +0100

    Add files via upload

---
+++ ProgramFuncional/src/Mayor10.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Mayor10 {
+
+	public static void main(String[] args) {
+
+
+		List<Integer> numbers=List.of(18,6,4,12,55,78,12);
+
+		Long Mayor10 = numbers.stream().filter(l->l>10).count();
+		
+		System.out.println(Mayor10);
+
+	}
+
+}
